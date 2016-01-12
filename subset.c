c72bb9a3caeb8a7d524337af6e9b213b57e24b38
SwathiSankar <SwathiSankar@users.noreply.github.com>
2016-01-12 17:25:33 +0530

    Update subset.c

--- subset.c
+++ subset.c
@@ -3,31 +3,35 @@ void main()
 {
 int n,i,y,k,j,z,a[50],x;
 
-printf("Enter the size of main set :");
-scanf(“%d”,&n);
+printf("Enter the size  :");
+scanf("%d",&n);
 
-printf("Enter the elements of main set :");
+printf("Enter the elements:");
 for(x=0;x<n;x++)
-scanf(“%d”,&a[x]);
+scanf("%d",&a[x]);
 
 printf("The subsets are :\n");
 
 for(i=1;i<=n;i++)
-{
-for(j=0;j<=n-i;j++)
-{
-if(i==1)
-printf("%d\n",a[j]);
-else
-{
-z=j+i-1;
-for(k=z;k<n;k++)
-{
-for(y=j;y<z;y++)
-printf("%d",a[y]);
-printf("%d\n",a[k]);
-}
-}
-}
-}
+ {
+  for(j=0;j<=n-i;j++)
+  {
+   if(i==1)
+   {
+    printf("%d\n",a[j]);
+   }
+   else
+   {
+    z=j+i-1;
+    for(k=z;k<n;k++)
+     {
+      for(y=j;y<z;y++)
+      {
+        printf("%d",a[y]);
+      }
+        printf("%d\n",a[k]);
+     }
+   }
+  }
+ }
 }
