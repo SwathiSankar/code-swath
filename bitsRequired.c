593b8a26c420db1d054f7ecdb428c92e96b49fa7
SwathiSankar <SwathiSankar@users.noreply.github.com>
2016-01-26 15:13:02 +0530

    Create bitsRequired.c

---
+++ bitsRequired.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+     int n,count=0;
+     printf("Enter the number \n");
+     scanf("%d",&n);
+     
+       if(n==0)
+       {
+             printf("1");
+       }
+    
+       while(n>0)
+       {
+       	     n=n/2;
+	           count++;
+       }
+
+     printf("%d",count);
+
+     return 0;
+}
