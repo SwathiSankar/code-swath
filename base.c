1961ad83ff106a06f9a45fa7c86fd268c6fde7a5
SwathiSankar <SwathiSankar@users.noreply.github.com>
2016-01-27 22:08:30 +0530

    Update base.c

--- base.c
+++ base.c
@@ -3,7 +3,7 @@
 int main()
 {
      int num,req_num,base;
-    // printf("Enter the number");
+     printf("Enter the number and the base \n");
      scanf("%d %d",&num,&base);
      
      conversion(num,base);
@@ -19,16 +19,16 @@ void conversion(int num,int base)
       int i=0,c=0,a[10];  
       while(num!=0)
       {
-          a[i] = num % base;
-          num = num / base;
-          i++;
-          c++;
+              a[i] = num % base;
+              num = num / base;
+              i++;
+              c++;
 
       }
 
     for(i=c-1;i>=0;i--)
     {
-     if(a[i]<=10)
+                if(a[i]<=10)
 		{
 			printf("%c",a[i]+'0');
 		}
