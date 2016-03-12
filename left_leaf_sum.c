e098aecb65be690eb0106c3520c70ce46c57a75f
SwathiSankar <SwathiSankar@users.noreply.github.com>
2016-03-12 11:25:34 +0530

    Update left_leaf_sum.c

--- left_leaf_sum.c
+++ left_leaf_sum.c
@@ -1,3 +1,13 @@
+//
+//  leftlesfsum.c
+//  
+//
+//  Created by R.M.D. Engineering College  on 12/03/16.
+//
+//
+
+#include <stdio.h>
+
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -5,58 +15,72 @@ int sum=0;
 
 struct bintree
 {
-	  int data;
-	  struct bintree *left;
-	  struct bintree *right;
+    int data;
+    struct bintree *left;
+    struct bintree *right;
 };
 
 struct bintree *newnode(int data)
 {
-	    struct bintree *node=(struct bintree*)malloc(sizeof(struct bintree));
+    struct bintree *node=(struct bintree*)malloc(sizeof(struct bintree));
+    
+    node->data=data;
+    
+    node->left=NULL;
+    node->right=NULL;
+    
+    return node;
+}
 
-	    node->data=data;
-
-	    node->left=NULL;
-	    node->right=NULL;
-
-	return node;
+int isleaf(struct bintree *node)
+{
+    
+        if(node!=NULL&&(node->left==NULL)&&(node->right == NULL))
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+   
 }
 
 
 int leftsum(struct bintree *node)
- {
-        if(node!=NULL)
+{
+    if(node!=NULL)
+    {
+        if(isleaf(node->left))
         {
-                  if((node->left==NULL)&&(node->right == NULL))
-                  {
-                          sum=sum+node->data;
-                  }
-
-
-                  leftsum(node->right);
-                  leftsum(node->left);
-
-       }
-
-
-
-	    return sum;
+            sum=sum+node->left->data;
+        }
+        
+        
+        leftsum(node->right);
+        leftsum(node->left);
+        
+    }
+    
+    
+    
+    return sum;
 }
 
 
 int main()
 {
-
-	      struct bintree *root=newnode(6);
-	      root->left=newnode(3);
-	      root->right=newnode(9);
-	      root->left->left=newnode(2);
-	      root->left->right=newnode(1);
-	      root->right->left=newnode(8);
-
-        sum=leftsum(root);
-
-	      printf("%d",sum);
-
-	      return 0;
+    
+    struct bintree *root=newnode(6);
+    root->left=newnode(3);
+    root->right=newnode(9);
+    root->left->left=newnode(13);
+    root->left->right=newnode(1);
+    root->right->left=newnode(7);
+    
+    sum=leftsum(root);
+    
+    printf("%d",sum);
+    
+    return 0;
 }
